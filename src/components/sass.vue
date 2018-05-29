469ef10d2d341e9d7dfdd11f7ff65206cdc36b00
386857560@qq.com <386857560@qq.com>
2018-05-29 23:01:21 +0800

    正常运行;

--- src/components/sass.vue
+++ src/components/sass.vue
@@ -1,11 +1,31 @@
 <template>
-  <div class="hello-world">
-    sass
+  <div class="main">
+    typescript、sass、vue学习
+    <div class="next">{{msg}}</div>
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+interface vueData{
+  msg: string
+}
 export default Vue.extend({
-  name: 'sass'
+  name: 'sass',
+  data(): vueData {
+    return {
+      msg: '这是msg'
+    }
+  }
 })
 </script>
+<style lang="scss" scoped>
+@import '../assets/css/base.scss';
+.main {
+  font-size: 20px;
+  color: $mainColor;
+  .next{
+    font-size: 16px;
+    color: $errColor;
+  }
+}
+</style>
